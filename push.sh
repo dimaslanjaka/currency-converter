a226d460e78eddfc61fe4dd6e7e780bcd5628fef
dimaslanjaka <dimaslanjaka@gmail.com>
2020-02-19 02:24:06 +0700

    Changes

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 gdt=date '+%A %W %Y %X'
+git config --global user.name "dimaslanjaka"
+git config --global user.email "dimaslanjaka@gmail.com"
+git config --global github.user dimaslanjaka
+t=`cat key.txt`
+git config --global github.token $t
 git add *
 git commit -m "Changes ${gdt}"
 git push
