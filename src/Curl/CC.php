986910cf0a6c7f04403a3aeb67e0753c4ff10c6d
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-02-18 16:42:03 +0700

    fix schemas

--- src/Curl/CC.php
+++ src/Curl/CC.php
@@ -2,11 +2,11 @@
 
 namespace Curl;
 
-class CC
+class CC extends Curl
 {
-  private $baseURL = 'https://api.exchangerate-api.com';
-  private $pathURL = '/v4/latest/';
-  private $cachedir = __DIR__ . '/json';
+  private $ccURL = 'https://api.exchangerate-api.com';
+  private $ccPATH = '/v4/latest/';
+  private $ccDIR = __DIR__ . '/json';
   private $cur = null;
   function __construct()
   {
@@ -15,6 +15,11 @@ class CC
 
   function set($cur)
   {
+    if (is_string($cur)) {
+      throw new th("CURRENCY must be instance of string instead of " . gettype($cur));
+    }
+    if ($cur == 'default') $cur = 'USD';
+    $this->cur = trim(strtoupper($cur));
   }
 
   function build($cur)
