a7adec38db62199035a693a84707f6882f6af90b
dimaslanjaka <dimaslanjaka@gmail.com>
2020-02-19 03:37:41 +0700

    Changes

--- push.sh
+++ push.sh
@@ -1,23 +1,25 @@
 #!/data/data/com.termux/files/usr/bin/sh
+am start -a android.intent.action.VIEW -d https://github.com/settings/tokens
 git config --global gui.encoding utf-8
 
 git config --global credential.helper store
+git config credential.helper store 
+
 git config --global user.name "dimaslanjaka"
 git config --global user.email "dimaslanjaka@gmail.com"
 git config --global github.user dimaslanjaka
 #git config --global user.signingkey 1DEDA67CD4106FF5
 t=`cat key.txt`
 git config --global github.token $t
-curl -u dimaslanjaka:$t https://api.github.com/user
-curl -v -H "Authorization: token ${t}" https://api.github.com/user/issues
+#curl -u dimaslanjaka:$t https://api.github.com/user
+#curl -v -H "Authorization: token ${t}" https://api.github.com/user/issues
 
 git remote -v 
 git remote remove origin 
 git remote add origin https://github.com/dimaslanjaka/currency-converter.git
 
 git add --all
-gdt=date
-git commit -m "Changes ${gdt}"
+git commit -m "Changes ${date}"
 #git rebase origin/master
 #git pull
 #git push -u origin master
