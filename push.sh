d2c9ff2a72e3f489ae1a9413897d4c0dcfdc9087
dimaslanjaka <dimaslanjaka@gmail.com>
2020-02-19 03:14:04 +0700

    Changes date

--- push.sh
+++ push.sh
@@ -1,8 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/sh
 git config --global gui.encoding utf-8
-git remote add origin https://github.com/dimaslanjaka/currency-converter.git
-git remote -v
-git add --all
+
 git config --global credential.helper store
 gdt=date
 git config --global user.name "dimaslanjaka"
@@ -11,6 +9,11 @@ git config --global github.user dimaslanjaka
 git config --global user.signingkey 1DEDA67CD4106FF5
 t=`cat key.txt`
 git config --global github.token $t
+
+git remote -v 
+git remote remove origin 
+git remote add origin git@github.com:dimaslanjaka/currency-converter.git
+git add --all
 git commit -m "Changes ${gdt}"
 git rebase origin/master
 git pull
