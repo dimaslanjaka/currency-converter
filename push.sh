8dac5ddecf067e4a140110e4b8aad2d5524f15b0
dimaslanjaka <dimaslanjaka@gmail.com>
2020-02-19 03:14:29 +0700

    Changes date

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ git config --global github.token $t
 
 git remote -v 
 git remote remove origin 
-git remote add origin git@github.com:dimaslanjaka/currency-converter.git
+git remote add origin https://github.com/dimaslanjaka/currency-converter.git
 git add --all
 git commit -m "Changes ${gdt}"
 git rebase origin/master
