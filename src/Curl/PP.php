11b024d3bad9217e3088bf67a9360ebfb9caf88f
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-02-21 00:37:30 +0700

    fx

--- src/Curl/PP.php
+++ src/Curl/PP.php
@@ -5,7 +5,9 @@ namespace Curl;
 class PP extends Curl
 {
   public $config_file;
-  private $pp_url = 'https://www.paypal.com/myaccount/money/api/currencies/transfer';
+  private $pp_url = '/myaccount/money/api/currencies/transfer';
+  private $paypal = 'https://www.paypal.com';
+  private $ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36';
 
   public function __construct($config_file = null)
   {
@@ -23,8 +25,31 @@ class PP extends Curl
     return $str[0];
   }
 
-  function pp_send($url, $h, $body)
+  function pp_send($path, $h, $body)
   {
+    //$this->instance = new Curl($this->paypal);
+    $this->setUrl($this->paypal);
+    $this->setOpt(CURLOPT_HTTPHEADER, $h);
+    $this->post($path, $body);
+  }
+
+  function old_request($url, $h, $body)
+  {
+    if (!$this->isurl($url)) $url = $this->paypal . $url;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $x = curl_exec($ch);
+    curl_close($ch);
+    return $x;
+  }
+
+  function isurl($url)
+  {
+    return filter_var($url, FILTER_VALIDATE_URL);
   }
 
   /**
@@ -35,24 +60,52 @@ class PP extends Curl
    *
    * @return json_decode
    */
-  public function usd_to_twd($cookie, $csrf)
+  public function usd_to_twd($cookie, $csrf, $ammount = false)
   {
     $arr = ["\r", '	'];
     $url = $this->pp_url;
-    $h = explode("\n", str_replace($arr, '', "Cookie: $cookie
+    /**
+     * $h = explode("\n", str_replace($arr, '', "Cookie: $cookie
 	Content-Type: application/json
 	user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"));
-    $body = "{\"sourceCurrency\":\"USD\",\"sourceAmount\":0.02,\"targetCurrency\":\"TWD\",\"_csrf\":\"$csrf\"}";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $x = curl_exec($ch);
-    curl_close($ch);
+     */
+    //$body = "{\"sourceCurrency\":\"USD\",\"sourceAmount\":0.02,\"targetCurrency\":\"TWD\",\"_csrf\":\"$csrf\"}";
+    $h = [
+      'Content-Type: application/json',
+      "Cookie: $cookie",
+      "user-agent: " . $this->ua
+    ];
+    if (!is_numeric($ammount)) {
+      echo "Ammount of " . __FUNCTION__ . " is ({$ammount}) invalid number\n";
+      $ammount = 0.02;
+      echo "Set ammount default {$ammount}\n";
+    }
+    $body = $this->gbody([
+      'csrf' => $csrf,
+      'src' => 'USD',
+      'to' => 'TWD',
+      'ammount' => $ammount
+    ]);
+    $x = $this->old_request($url, $h, $body);
 
     return json_decode($x, true);
+  }
+
+  /**
+   * Build Body Post.
+   *
+   * @param array $cfg
+   *
+   * @return string
+   */
+  function gbody($cfg)
+  {
+    return gjson([
+      'sourceCurrency' => trim(strtoupper($cfg['src'])),
+      'sourceAmount' => trim($cfg['ammount']),
+      'targetCurrency' => trim(strtoupper($cfg['to'])),
+      '_csrf' => trim($cfg['csrf']),
+    ]);
   }
 
   /**
@@ -71,14 +124,7 @@ class PP extends Curl
 	Content-Type: application/json
 	user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"));
     $body = "{\"sourceCurrency\":\"TWD\",\"sourceAmount\":3,\"targetCurrency\":\"USD\",\"_csrf\":\"$csrf\"}";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $x = curl_exec($ch);
-    curl_close($ch);
+    $x = $this->old_request($url, $h, $body);
 
     return json_decode($x, true);
   }
@@ -99,14 +145,7 @@ class PP extends Curl
 	Content-Type: application/json
 	user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"));
     $body = "{\"sourceCurrency\":\"JPY\",\"sourceAmount\":2,\"targetCurrency\":\"TWD\",\"_csrf\":\"$csrf\"}";
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $x = curl_exec($ch);
-    curl_close($ch);
+    $x = $this->old_request($url, $h, $body);
 
     return json_decode($x, true);
   }
---
+++ src/Curl/console.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Curl;
+
+/**
+ * Usage:
+// Output screenshot:
+// http://cl.ly/NsqF
+// -------------------------------------------------------
+
+include_once 'console.php';
+
+// ::log method usage
+// -------------------------------------------------------
+Console::log('Im Red!', 'red');
+Console::log('Im Blue on White!', 'white', true, 'blue');
+
+Console::log('I dont have an EOF', false);
+Console::log("\tThis is where I come in.", 'light_green');
+Console::log('You can swap my variables', 'black', 'yellow');
+Console::log(str_repeat('-', 60));
+
+// Direct usage
+// -------------------------------------------------------
+echo Console::blue('Blue Text') . "\n";
+echo Console::black('Black Text on Magenta Background', 'magenta') . "\n";
+echo Console::red('Im supposed to be red, but Im reversed!', 'reverse') . "\n";
+echo Console::red('I have an underline', 'underline') . "\n";
+echo Console::blue('I should be blue on light gray but Im reversed too.', 'light_gray', 'reverse') . "\n";
+
+// Ding!
+// -------------------------------------------------------
+echo Console::bell();
+ */
+/**
+ * PHP Colored CLI
+ * Used to log strings with custom colors to console using php.
+ *
+ * Copyright (C) 2013 Sallar Kaboli <sallar.kaboli@gmail.com>
+ * MIT Liencesed
+ * http://opensource.org/licenses/MIT
+ *
+ * Original colored CLI output script:
+ * (C) Jesse Donat https://github.com/donatj
+ */
+class Console
+{
+  public static $foreground_colors = [
+    'bold' => '1',    'dim' => '2',
+    'black' => '0;30', 'dark_gray' => '1;30',
+    'blue' => '0;34', 'light_blue' => '1;34',
+    'green' => '0;32', 'light_green' => '1;32',
+    'cyan' => '0;36', 'light_cyan' => '1;36',
+    'red' => '0;31', 'light_red' => '1;31',
+    'purple' => '0;35', 'light_purple' => '1;35',
+    'brown' => '0;33', 'yellow' => '1;33',
+    'light_gray' => '0;37', 'white' => '1;37',
+    'normal' => '0;39',
+  ];
+
+  public static $background_colors = [
+    'black' => '40',   'red' => '41',
+    'green' => '42',   'yellow' => '43',
+    'blue' => '44',   'magenta' => '45',
+    'cyan' => '46',   'light_gray' => '47',
+  ];
+
+  public static $options = [
+    'underline' => '4',    'blink' => '5',
+    'reverse' => '7',    'hidden' => '8',
+  ];
+
+  public static $EOF = "\n";
+
+  public static function warna($text, $warna)
+  {
+    $warna = strtoupper($warna);
+    $list = [];
+    $list['BLACK'] = "\033[30m";
+    $list['RED'] = "\033[31m";
+    $list['GREEN'] = "\033[32m";
+    $list['YELLOW'] = "\033[33m";
+    $list['BLUE'] = "\033[34m";
+    $list['MAGENTA'] = "\033[35m";
+    $list['CYAN'] = "\033[36m";
+    $list['WHITE'] = "\033[37m";
+    $list['RESET'] = "\033[39m";
+    $warna = $list[$warna];
+    $reset = $list['RESET'];
+    if (in_array($warna, $list)) {
+      $text = "$warna$text$reset";
+    } else {
+      $text = $text;
+    }
+
+    return $text;
+  }
+
+  /**
+   * Logs a string to console.
+   *
+   * @param string $str        Input String
+   * @param string $color      Text Color
+   * @param bool   $newline    Append EOF?
+   * @param [type] $background Background Color
+   *
+   * @return [type] Formatted output
+   */
+  public static function log($str = '', $color = 'normal', $newline = true, $background_color = null)
+  {
+    if (is_bool($color)) {
+      $newline = $color;
+      $color = 'normal';
+    } elseif (is_string($color) && is_string($newline)) {
+      $background_color = $newline;
+      $newline = true;
+    }
+    $str = $newline ? $str . self::$EOF : $str;
+
+    echo self::$color($str, $background_color);
+  }
+
+  /**
+   * Anything below this point (and its related variables):
+   * Colored CLI Output is: (C) Jesse Donat
+   * https://gist.github.com/donatj/1315354
+   * -------------------------------------------------------------.
+   */
+
+  /**
+   * Catches static calls (Wildcard).
+   *
+   * @param string $foreground_color Text Color
+   * @param array  $args             Options
+   *
+   * @return string Colored string
+   */
+  public static function __callStatic($foreground_color, $args)
+  {
+    $string = $args[0];
+    if ('WIN' === strtoupper(substr(PHP_OS, 0, 3))) {
+      return $string;
+    } else {
+      $colored_string = '';
+
+      // Check if given foreground color found
+      if (isset(self::$foreground_colors[$foreground_color])) {
+        $colored_string .= "\033[" . self::$foreground_colors[$foreground_color] . 'm';
+      } else {
+        die($foreground_color . ' not a valid color');
+      }
+
+      array_shift($args);
+
+      foreach ($args as $option) {
+        // Check if given background color found
+        if (isset(self::$background_colors[$option])) {
+          $colored_string .= "\033[" . self::$background_colors[$option] . 'm';
+        } elseif (isset(self::$options[$option])) {
+          $colored_string .= "\033[" . self::$options[$option] . 'm';
+        }
+      }
+
+      // Add string and end coloring
+      $colored_string .= $string . "\033[0m";
+
+      return $colored_string;
+    }
+  }
+
+  /**
+   * Plays a bell sound in console (if available).
+   *
+   * @param int $count Bell play count
+   *
+   * @return string Bell play string
+   */
+  public static function bell($count = 1)
+  {
+    echo str_repeat("\007", $count);
+  }
+}
