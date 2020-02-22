2d95509ef878b1b5c6202aa95769cc3bbc6485cc
Dimas Lanjaka <dimaslanjaka@gmail.com>
2020-02-22 16:43:50 +0700

    fx

--- pp/bin/install.sh
+++ pp/bin/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-curl -sS https://raw.githubusercontent.com/dimaslanjaka/currency-converter/master/pp/bin/bot | php -- --install-dir=/data/data/com.termux/files/usr/bin --filename=bot
+curl -sS https://raw.githubusercontent.com/dimaslanjaka/currency-converter/master/pp/bin/bot | php -- --install --install-dir=/data/data/com.termux/files/usr/bin --filename=bot
 chmod 777 /data/data/com.termux/files/usr/bin/bot
